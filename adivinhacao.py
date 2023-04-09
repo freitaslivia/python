06d448aa05362715a5339bdd73f83c6c0b006427
freitaslivia <xliviafreitasx@gmail.com>
2023-04-09 00:51:09 -0300

    .format, while e for

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,29 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_tentativas = 3
 
-chute = int(input("Digite o seu número: "))
 
-print("voce digitou" , chute)
+# while (rodada <= total_tentativas):
+#     print("Tentativa {} de {}".format(rodada, total_tentativas))
+#     chute = int(input("Digite o seu número: "))
+#     print("voce digitou" , chute)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("voce digitou" , chute)
 
-if (acertou):
-    print("Você acertou")
-else: 
-    if(maior):
-        print ("O seu chute foi maior do que o número secreto")
-    elif(menor):
-        print ("O seu chute foi menor do que o número secreto")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
+    if (acertou):
+        print("Você acertou")
+    else: 
+        if(maior):
+            print ("O seu chute foi maior do que o número secreto")
+        elif(menor):
+            print ("O seu chute foi menor do que o número secreto")
+    
 print("Fim do jogo")
