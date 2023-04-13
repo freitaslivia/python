598cd30e9ef0818ebb1581133f75a78e6cad265e
freitaslivia <xliviafreitasx@gmail.com>
2023-04-12 22:58:28 -0300

    break e cotinue Alura

--- adivinhacao.py
+++ adivinhacao.py
@@ -12,9 +12,13 @@ total_tentativas = 3
 #     print("voce digitou" , chute)
 
 for rodada in range(1, total_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_tentativas))
-    chute = int(input("Digite o seu número: "))
+    print("Tentativa {} de {}".format (rodada, total_tentativas))
+    chute = int(input("Digite um número entre 1 e 100\n>"))
     print("voce digitou" , chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um npumero entre 1 e 100! ")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -22,6 +26,7 @@ for rodada in range(1, total_tentativas + 1):
 
     if (acertou):
         print("Você acertou")
+        break
     else: 
         if(maior):
             print ("O seu chute foi maior do que o número secreto")
