e8da4cc9d76f1cea9157e4f72f73b0376ad1a7a1
freitaslivia <xliviafreitasx@gmail.com>
2023-05-01 21:05:17 -0300

    tarefa 30 não terminada.

---
+++ lista/tarefa30.py
@@ -0,0 +1,17 @@
+import random
+
+num = int(input("Digite um número\n>"))
+
+lista = []
+
+for i in range(100):
+    num_aleatorio = random.randrange(1, 100)
+    lista.append(num_aleatorio)
+    if num == num_aleatorio:
+        print("O número que você digitou está na lista")
+        lista(num)
+        break
+else:
+    print("O número que você digitou não está na lista")
+
+print(lista)
